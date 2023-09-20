e32f28fd3b31838b86ee79d6eefde1bddbb59f70
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-09-20 07:27:08 -0300

    Problem with orbit definition solved!! example_MiyamotoNagai run, remains to test precision

--- src/evolutions.jl
+++ src/evolutions.jl
@@ -3,17 +3,14 @@
 """Evolution of AbstractPotential"""
 function evolve(pot::AbstractPotential, x::Vector{<:U.Length}, v::Vector{<:U.Velocity},
     t_span::Tuple{<:U.Time, <:U.Time})
-dimₓ, dimᵥ = unit(x[begin]), unit(v[begin])
-x = ustrip(uconvert.(u_L, x))
-v = ustrip(uconvert.(u_V, v))
-t_span = uconvert.(unit(u_T),t_span)./u_T
-p = pot
-u₀ = SA[x...,v...]
-prob = ODEProblem(ode, u₀, t_span, p)
-sol=solve(prob, Vern9(); abstol=5.e-8, reltol=5.e-8)
-@show sol.u[67][3]
-@show typeof(sol.u[67])
-@show size(sol.u[1:3,:])
-orb = Orbit(sol.t*u_T, sol.u[1:3,:]*u_L, sol.u[4:6,:]*u_V)
-return orb
+    dimₓ, dimᵥ = unit(x[begin]), unit(v[begin])
+    x = ustrip(uconvert.(u_L, x))
+    v = ustrip(uconvert.(u_V, v))
+    t_span = uconvert.(unit(u_T),t_span)./u_T
+    p = pot
+    u₀ = SA[x...,v...]
+    prob = ODEProblem(ode, u₀, t_span, p)
+    sol=solve(prob, Vern9(); abstol=5.e-8, reltol=5.e-8)
+    orb = Orbit(sol.t*u_T, sol[1:3,:]*u_L, sol[4:6,:]*u_V)
+    return orb
 end
