8d8877a9ff6ffdd8bf7a7ac5499fa40026d331f5
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-10-10 13:34:03 -0300

    update test

--- test/acceleration/test_accelerations.jl
+++ test/acceleration/test_accelerations.jl
@@ -13,7 +13,7 @@ accelerations_py = pyimport("accelerations")
     γ = 2.0
     pot = GalacticDynamics.AllenSantillanHalo(m, a, Λ, γ)
     pot_py = accelerations_py.AllenSantillan(ustrip.([m, a, Λ, γ])...)
-    for i in range(1,200)
+    for i in range(1,100)
         x = 50*rand(3)*u"kpc"
         @test ustrip.(acceleration(pot,x)) ≈ pyconvert(Vector{Float64},pot_py.accel(ustrip.(x)...)) rtol=5.e-6
     end
@@ -21,12 +21,12 @@ end
 
 @testset "MiyamotoNagaiDisk" begin
     m_gal = 2.325e7*u"Msun"
-    m =1018.0*m_gal  # Msun
-    a = 10.0*u"kpc"     # kpc
-    b = 3.0*u"kpc"    # kpc
+    m =500.0*m_gal  # Msun
+    a = 12.0*u"kpc"     # kpc
+    b = 4.0*u"kpc"    # kpc
     pot = GalacticDynamics.MiyamotoNagaiDisk(m, a, b)
     pot_py = accelerations_py.MiyamotoNagai(ustrip.([m, a, b])...)
-    for i in range(1,200)
+    for i in range(1,100)
         x = 50*rand(3)*u"kpc"
         @test ustrip.(acceleration(pot,x)) ≈ pyconvert(Vector{Float64},pot_py.accel(ustrip.(x)...)) rtol=5.e-6
     end
