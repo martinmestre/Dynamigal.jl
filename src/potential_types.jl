c8bea4d5a204a25d7ffdb6763ef64b841fd81bce
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-11-18 17:01:02 -0300

    Finished refactoring potential_types.jl Remains the rest of the code.

--- src/potential_types.jl
+++ src/potential_types.jl
@@ -3,15 +3,15 @@
 @with_kw struct PointMass{T} <: AbstractPotential
     m::T
 end
-PointMass(m::T) where {T<:Unitful.Mass} = PointMass( ustrip(uconvert(u_M, m)) )
+    PointMass(m::T) where {T<:U.Mass} = PointMass( ustrip(uconvert(u_M, m)) )
 
 
 @with_kw struct Plummer{T,D} <: AbstractPotential
     m::T
     b::D
 end
-Plummer(m::T, b::D) where {T<:Unitful.Mass, D<:Unitful.Length} =
-Plummer( ustrip(uconvert(u_M, m)),  ustrip(uconvert(u_L, b)) )
+Plummer(m::T, b::D) where {T<:U.Mass, D<:U.Length} =
+    Plummer( ustrip(uconvert(u_M, m)),  ustrip(uconvert(u_L, b)) )
 
 
 @with_kw struct MiyamotoNagaiDisk{T,D,F} <: AbstractDiskPotential
@@ -19,25 +19,16 @@ Plummer( ustrip(uconvert(u_M, m)),  ustrip(uconvert(u_L, b)) )
     a::D
     b::F
 end
-MiyamotoNagaiDisk(m::T, a::D, b::F) where {T<:Unitful.Mass, D<:Unitful.Length, F<:Unitful.Length} =
-MiyamotoNagaiDisk( ustrip(uconvert(u_M, m)),  ustrip(uconvert(u_L, a)), ustrip(uconvert(u_L, b)) )
+MiyamotoNagaiDisk(m::T, a::D, b::F) where {T<:U.Mass, D<:U.Length, F<:U.Length} =
+    MiyamotoNagaiDisk( ustrip(uconvert(u_M, m)),  ustrip(uconvert(u_L, a)), ustrip(uconvert(u_L, b)) )
 
 
-@with_kw struct AllenSantillanHalo{M<:U.Mass, L<:U.Length, D<:Real, T<:Real} <: AbstractDiskPotential
-    m_u::M
-    a_u::L
-    Λ_u::L
-    γ::D
+@with_kw struct AllenSantillanHalo{T,D,F,G} <: AbstractDiskPotentia
     m::T
-    a::T
-    Λ::T
-    function AllenSantillanHalo{M,L,D}(m_u, a_u, Λ_u, γ) where {M,L,D}
-        m = uconvert(u_M, m_u).val
-        a = uconvert(u_L, a_u).val
-        Λ = uconvert(u_L, Λ_u).val
-        T=typeof(m)
-        return new{M,L,D,T}(m_u, a_u, Λ_u, γ, m, a, Λ)
-    end
+    a::D
+    Λ::F
+    γ::G
 end
-AllenSantillanHalo(m_u::M, a_u::L, Λ_u::L, γ::D) where {M,L,D} = AllenSantillanHalo{M,L,D}(m_u, a_u, Λ_u, γ)
-AllenSantillanHalo(m::T, a::D, Λ::F, γ::G) where {T<:Real,D<:Real,F<:Real,G<:Real} = AllenSantillanHalo(m*u_M, a*u_L, Λ*u_L, γ)
+AllenSantillanHalo(m::T, a::D, Λ::F, γ::G) where {T<:U.Mass, D<:U.Length, F<:U.Length, G<:Real} =
+    AllenSantillanHalo( ustrip(uconvert(u_M, m)),  ustrip(uconvert(u_L, a)),
+                        ustrip(uconvert(u_L, Λ)),  γ )
