425ee32953b8706d94baa8a162e5d981d74aa28d
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-11-18 19:30:18 -0300

    accel edit

--- src/accelerations.jl
+++ src/accelerations.jl
@@ -1,25 +1,31 @@
 """Acceleration of AbstractPotentials"""
 
 
-"""Acceleration"""
-function acceleration(pot::AbstractPotential, x::AbstractArray{T}) where {T<:Real}
-    return -1.0*gradient(y->potential(pot, y), x)[1]
-end
-
 """Unitful acceleration"""
-function acceleration(pot::UnionAbstractPotentials, x::Vector{<:U.Length})
-    x = ustrip(uconvert.(u_L, x))
-    return u_A*acceleration(pot, x)
+function acceleration(pot::UnionAbstractPotentials, x::Vector{<:Unitful.Length}; kargs...)
+    x = ustrip(uconvert.(lu.l, x))
+    return acceleration(pot, x; kargs...)*lu.a
 end
 
-"""Acceleration in a sum of AbstractPotentials"""
+
+"""Acceleration of a sum of AbstractPotentials"""
 function acceleration(pot::Vector{<:AbstractPotential}, x::AbstractArray{T}) where {T<:Real}
     sum_acc = zeros(3)
     for i ∈ eachindex(pot)
-        sum_acc .+= acceleration(pot[i], x)
+        sum_acc .+= acceleration(pot[i], x; kwargs...)
     end
     return sum_acc
 end
+
+
+"""Acceleration of single potential"""
+function acceleration(pot::AbstractPotential, x::AbstractArray{T}; kargs...) where {T<:Real}
+    return -1.0*gradient(y->potential(pot, y; kwargs...), x)[1]
+end
+
+
+
+
 
 
 
