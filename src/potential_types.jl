4291ecf621031e139a2b3234fa413a67f2f15cdd
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-11-18 15:39:46 -0300

    Refactoring types

--- src/potential_types.jl
+++ src/potential_types.jl
@@ -1,49 +1,26 @@
 """Potential types"""
 
-@with_kw struct PointMass{M<:U.Mass, T<:Real} <: AbstractPotential
-    m_u::M
+@with_kw struct PointMass{T} <: AbstractPotential
     m::T
-    function PointMass{M}(m_u) where {M}
-        m = uconvert(u_M, m_u).val
-        T=typeof(m)
-        return new{M,T}(m_u, m)
-    end
 end
-PointMass(m_u::M) where {M} = PointMass{M}(m_u)
-PointMass(m::T) where {T<:Real} = PointMass(m*u_M)
+PointMass(m::T) where {T<:Unitful.Mass} = PointMass( ustrip(uconvert(u_M, m)) )
 
-@with_kw struct Plummer{M<:U.Mass, L<:U.Length, T<:Real} <: AbstractPotential
-    m_u::M
-    b_u::L
-    m::T
-    b::T
-    function Plummer{M,L}(m_u, b_u) where {M,L}
-        m = uconvert(u_M, m_u).val
-        b = uconvert(u_L, b_u).val
-        T=typeof(m)
-        return new{M,L,T}(m_u, b_u, m, b)
-    end
-end
-Plummer(m_u::M, b_u::L) where {M,L} = Plummer{M,L}(m_u, b_u)
-Plummer(m::T, b::D) where {T<:Real,D<:Real} = Plummer(m*u_M, b*u_L)
 
-@with_kw struct MiyamotoNagaiDisk{M<:U.Mass, L<:U.Length, T<:Real} <: AbstractDiskPotential
-    m_u::M
-    a_u::L
-    b_u::L
+@with_kw struct Plummer{T,D} <: AbstractPotential
     m::T
-    a::T
-    b::T
-    function MiyamotoNagaiDisk{M,L}(m_u, a_u, b_u) where {M,L}
-        m = uconvert(u_M, m_u).val
-        a = uconvert(u_L, a_u).val
-        b = uconvert(u_L, b_u).val
-        T=typeof(m)
-        return new{M,L,T}(m_u, a_u, b_u, m, a, b)
-    end
+    b::D
 end
-MiyamotoNagaiDisk(m_u::M, a_u::L, b_u::L) where {M,L} = MiyamotoNagaiDisk{M,L}(m_u, a_u, b_u)
-MiyamotoNagaiDisk(m::T, a::D, b::F) where {T<:Real, D<:Real, F<:Real} = Plummer(m*u_M, a*u_L, b*u_L)
+Plummer(m::T, b::D) where {T<:Unitful.Mass, D<:Unitful.Length} =
+Plummer( ustrip(uconvert(u_M, m)),  ustrip(uconvert(u_L, b)) )
+
+
+@with_kw struct MiyamotoNagaiDisk{T,D,F} <: AbstractDiskPotential
+    m::T
+    a::D
+    b::F
+end
+MiyamotoNagaiDisk(m::T, a::D, b::F) where {T<:Unitful.Mass, D<:Unitful.Length, F<:Unitful.Length} =
+MiyamotoNagaiDisk( ustrip(uconvert(u_M, m)),  ustrip(uconvert(u_L, a)), ustrip(uconvert(u_L, b)) )
 
 
 @with_kw struct AllenSantillanHalo{M<:U.Mass, L<:U.Length, D<:Real, T<:Real} <: AbstractDiskPotential
